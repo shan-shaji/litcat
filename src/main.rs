ab2f1070936bc112bd12dbe324910aedd6684eea
shan-shaji <abrahamshanshaji@gmail.com>
2025-07-11 22:34:22 +0200

    feat: read file and print with color

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,31 @@
 use clap::Parser;
+use colored::Colorize;
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+};
 
 #[derive(Parser)]
+#[command (author, version, about, long_about = None)]
 struct Args {
+    /// Path to the file to display
     file: String,
 }
 
-fn main() {
-    println!("Hello, world!");
+fn main() -> std::io::Result<()> {
+    let args = Args::parse();
+    let file = File::open(&args.file)?;
+    let _reader = BufReader::new(file);
+
+    for line_result in _reader.lines() {
+        let line = line_result?;
+        if line.starts_with('+') && !line.starts_with("+++") {
+            println!("{}", line.green());
+        } else if line.starts_with('-') && !line.starts_with("---") {
+            println!("{}", line.red());
+        } else {
+            println!("{}", line);
+        }
+    }
+    Ok(())
 }
