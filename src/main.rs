f7cedd680c2d31b8864657bff3a133679b83f14d
shan-shaji <abrahamshanshaji@gmail.com>
2025-07-12 16:25:35 +0200

    feat: add support to accept `stdin` input

--- src/main.rs
+++ src/main.rs
@@ -2,24 +2,29 @@ use clap::Parser;
 use colored::Colorize;
 use std::{
     fs::File,
-    io::{BufRead, BufReader},
+    io::{BufRead, BufReader, stdin},
 };
+
+enum InputSource {
+    File(BufReader<File>),
+    Stdin(BufReader<std::io::Stdin>),
+}
 
 #[derive(Parser)]
 #[command (author, version, about, long_about = None)]
 struct Args {
-    /// Path to the file to display
-    file: String,
+    /// Path to the file to display. If not provided, reads from stdin.
+    file: Option<String>,
 }
 
-fn main() -> std::io::Result<()> {
-    let args = Args::parse();
-    let file = File::open(&args.file)?;
-    let _reader = BufReader::new(file);
-
-    for line_result in _reader.lines() {
+fn process_lines<R: BufRead>(reader: R) -> std::io::Result<()> {
+    for line_result in reader.lines() {
         let line = line_result?;
-        if line.starts_with('+') && !line.starts_with("+++") {
+        if line.starts_with("+++ ") || line.starts_with("--- ") {
+            println!("{}", line.cyan());
+        } else if line.starts_with("@@") {
+            println!("{}", line.yellow());
+        } else if line.starts_with('+') && !line.starts_with("+++") {
             println!("{}", line.green());
         } else if line.starts_with('-') && !line.starts_with("---") {
             println!("{}", line.red());
@@ -27,5 +32,19 @@ fn main() -> std::io::Result<()> {
             println!("{}", line);
         }
     }
+
     Ok(())
+}
+
+fn main() -> std::io::Result<()> {
+    let args = Args::parse();
+    let input = match args.file {
+        Some(path) => InputSource::File(BufReader::new(File::open(path)?)),
+        None => InputSource::Stdin(BufReader::new(stdin())),
+    };
+
+    match input {
+        InputSource::File(reader) => process_lines(reader),
+        InputSource::Stdin(reader) => process_lines(reader),
+    }
 }
